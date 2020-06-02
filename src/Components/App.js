a79fdfc198651f836e835c4d5e8a1a8771777a31
Daniel Kamiński <daniel.kaminski1998@gmail.com>
2020-06-02 12:45:47 +0200

    Theme Colors

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Navigation from './Nav';
 import Books from './LibraryComponents/Books';
@@ -19,36 +18,43 @@ import { UserProvider } from './UserComponents/UserContext';
 import EditBook from './LibraryComponents/EditBook';
 import BookDetails from './LibraryComponents/BookDetails';
 import OwnedBooks from './LibraryComponents/OwnedBooks';
-
+import { ThemeProvider } from '@material-ui/styles';
+import defaultTheme from './theme';
 function App() {
   return (
-    <UserProvider>
-      <Router>
-        <div className='App'>
-          <Navigation />
-          <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/books' exact component={Books} />
-            <Route path='/books/details/:id' component={BookDetails} />
-            <PrivateRoute path='/newBook' component={NewBook} />
-            <Route path='/login' component={Login} />
-            <ProtectedRoute path='/logout' component={Logout} />
-            <ProtectedRoute path='/myCollection' exact component={OwnedBooks} />
-            <ProtectedRoute path='/panel' exact component={AdminPanel} />
-            <ProtectedRoute path='/panel/newUser' component={NewUser} />
-            <ProtectedRoute
-              path='/myCollection/editBook/:id'
-              exact
-              component={EditBook}
-            />
-            <ProtectedRoute
-              path='/panel/deleteUser/:id'
-              component={DeleteUser}
-            />
-          </Switch>
-        </div>
-      </Router>
-    </UserProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <UserProvider>
+        <Router>
+          <div className='App'>
+            <Navigation />
+            <Switch>
+              <Route path='/' exact component={Home} />
+              <Route path='/books' exact component={Books} />
+              <Route path='/books/details/:id' component={BookDetails} />
+              <PrivateRoute path='/newBook' component={NewBook} />
+              <Route path='/login' component={Login} />
+              <ProtectedRoute path='/logout' component={Logout} />
+              <ProtectedRoute
+                path='/myCollection'
+                exact
+                component={OwnedBooks}
+              />
+              <ProtectedRoute path='/panel' exact component={AdminPanel} />
+              <ProtectedRoute path='/panel/newUser' component={NewUser} />
+              <ProtectedRoute
+                path='/myCollection/editBook/:id'
+                exact
+                component={EditBook}
+              />
+              <ProtectedRoute
+                path='/panel/deleteUser/:id'
+                component={DeleteUser}
+              />
+            </Switch>
+          </div>
+        </Router>
+      </UserProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Components/theme.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createMuiTheme, makeStyles } from '@material-ui/core/styles';
+const defaultTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#6C63FF',
+    },
+  },
+});
+export default defaultTheme;
